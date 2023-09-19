acfbf864a9713ce8aa828d0fc2ab6a9ec501129b
spydermnky <peterkhomchenk00@gmail.com>
2023-09-19 15:43:42 -0400

    What is your name?
    Added "What is your name query?"

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,5 +12,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.includes("What is your name?")) {
+    return (
+      "My name is Peter"
+    );
+  }
+
   return "";
 }
